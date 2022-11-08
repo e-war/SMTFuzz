55fb9fe35fac51b5a10293811e83fa73bfc42792
e-war <elliotwarren17@gmail.com>
2022-11-08 22:52:17 +0000

    all but timeout issue

--- smtfuzzer.py
+++ smtfuzzer.py
@@ -5,29 +5,67 @@
 # Written by Elliot Warren (github.com/e-war) @elliotwarren
 
 # WARNING: Will only work against targets without authorization for now until implemented
+
+# TODO:
+# FIX TIMEOUT ISSUE
+# refactor to seperate SMTP connection establishing so while the usernames are checked we can restart the connection at a saved place if we are disconnected.
+
 import sys
+import time
 import re
 from telnetlib import Telnet
-
+from os import system
 
 def invokeSMTPConnection(u,ip_p):
-    
+# Initialize variables
+    verified_users = []
+    expected_connection_codes = [b'250 ',b'220 ']
+# 1. Valid user 2. Invalid user 3. Timeout after too many failed connections
+    expected_user_codes = [b'252 2.0.0 ',b'550 5.1.1 ',b'421 4.7.0 ']
 # Initialize the connection
-    connection = Telnet(ip_p[0],ip_p[1],timeout=3)
+    connection = Telnet(ip_p[0],ip_p[1],timeout=5)
     connection.write(b'HELO '+bytes(ip_p[0],"utf-8")+b'\n')
 
-#Decode the correct hostname for VERIFICATION 
-
-    connection.read_until(b'250 ')  #additional space at the end
-    hostname = connection.read_until(b'\n').decode("ascii")
+# Decode the correct hostname for VERIFICATION 
+# This connection process takes a long time right now 
+    returned_connection_code = connection.expect(expected_connection_codes,timeout=30)
+    if(returned_connection_code[0] == -1):
+        raise ConnectionError
+    elif(returned_connection_code[0] == 0):
+        hostname = connection.read_until(b'\n').decode("ascii")
+    elif(returned_connection_code[0] == 1):
+        hostname = connection.read_until(b'\n').decode("ascii")
+        hostname = re.search(r"^[\w\d\.]+",hostname).group()
     
-#For username in user list, verify they exist, append to verified users
-    verified_users = []
+# BUG: NEED TO LET SERVER SETTLE AFTER CONNECT
+    time.sleep(3)
+
+# For username in user list, verify they exist, append to verified users
     for username in u:
-        connection.write(b'VRFY '+bytes(username,"utf-8")+b'@'+bytes(hostname,"utf-8")+b'\n')
-        connection.read_until(b'252 2.0.0 ')
-        verified_users.append(connection.read_until(b'\n').decode("ascii"))
-    print("Verified Users:",verified_users.count())
+        connection.write(b'VRFY "'+bytes(username,"utf-8")+b'@'+bytes(hostname,"utf-8")+b'"\n')
+        returned_user_code = connection.expect(expected_user_codes,timeout=3)
+# If timeout die
+        if(returned_user_code[0] == -1):
+            raise ConnectionError
+# If valid, add to verified
+        elif(returned_user_code[0] == 0):
+                verified_users.append(connection.read_until(b'\n').decode("ascii"))               
+# If invalid sleep to avoid DoS detection/timeout
+        elif(returned_user_code[0] == 1):
+                time.sleep(1)
+# If dos detection die 
+        elif(returned_user_code[0] == 2):
+            raise ConnectionError
+# Show a continous updated list of all verified usernames
+        system('/usr/bin/clear')
+        print("############ FUZZING "+hostname+" ####")
+        print("# Found",len(verified_users),"/(",u.index(username),"/",len(u),")")
+        print("# Please be patient, this program is slow:")
+        print("# Trying:",username)
+        print("# Verified Users:")
+        for user in verified_users:
+            print("#"+user)
+        
     connection.write(b'exit\n')
     return verified_users
 
@@ -36,6 +74,7 @@ def __main__():
 # Setup variables
     user_args=[]
     username_list = []
+    sanitized_usernames = []
     valid_usernames = []
     ip_port = ''
     wordlist_path = ''
@@ -67,21 +106,29 @@ def __main__():
     except FileNotFoundError:
         print("The file path was not valid, please try again")
         return 1
-        print("Verified user:", username)
+# Remove characters which break TELNET requests
+    for unsanitised_username in username_list:
+        if(re.match(r"[\&\’\`\*\|\/\$\!\@\:\%\(\)\?\"\=\%\{\}\[\]\.\#\~]",unsanitised_username)):
+            continue
+        else:
+            sanitized_usernames.append(unsanitised_username)
+    if(len(sanitized_usernames) == 0):
+        print("No valid usernames found in that file! Please try again.")
+        return 1
 # Invoke SMTP connection handler and send username list
 
-    valid_usernames = invokeSMTPConnection(username_list, ip_port)
+    try:
+        print("Establishing connection...")
+        valid_usernames = invokeSMTPConnection(sanitized_usernames, ip_port)
+    except ConnectionError:
+        print("A connection error such as timeout may have occured, please attemept again after re-establishing connection.")
+        return 1
 
 
-    if(valid_usernames.count() == 0):
+    if(len(valid_usernames) == 0):
         print("No valid usernames from your wordlist were found.")
     else:
         print("Here is a list of valid usernames i found:")
         for username in valid_usernames:
             print(username)
-
-
-    print(user_args)
-    print(username_list)
-
 __main__()
